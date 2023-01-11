3c90b4a10f69753274d00e05a67551d83637057d
mlietti1 <mcf.lietti@outlook.com>
2023-01-11 12:39:40 +0100

    slug-control

--- app/Models/Wine.php
+++ app/Models/Wine.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Wine extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($string){
+
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+
+        $wine_exists = Wine::where('slug', $slug)->first();
+
+        $c = 1;
+
+        while($wine_exists){
+
+
+            $slug = $original_slug . '-' . $c;
+            $c++;
+
+            $wine_exists = Wine::where('slug', $slug)->first();
+
+
+        }
+
+        return $slug;
+
+    }
 }
